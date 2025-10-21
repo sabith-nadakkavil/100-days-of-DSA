828e5578b8d961275b94474456063da157007988
Mohamed Sabith <47350620+sabith-nadakkavil@users.noreply.github.com>
2025-10-22 01:09:42 +0530

    Time: 4 ms (96.95%) | Memory: 45.2 MB (6.33%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (char c : moves.toCharArray()){
+            if ( c == 'U')
+                y += 1;
+            else if( c == 'D')
+                y -= 1;
+            else if (c == 'R')
+                x += 1;
+            else
+                x -= 1;
+        }
+        return x == 0 && y== 0;
+    }
+}
