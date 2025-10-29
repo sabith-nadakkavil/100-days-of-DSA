9848cf966aa2c6a25deeb227abc9de5d43fa08d7
Mohamed Sabith <47350620+sabith-nadakkavil@users.noreply.github.com>
2025-10-29 15:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (29.80%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int first = 1, second = 2, cur = 0;
+        for(int i = 3; i <= n; i++){
+            cur = first + second;
+            first = second;
+            second = cur;
+        }
+        return cur;
+    }
+}
