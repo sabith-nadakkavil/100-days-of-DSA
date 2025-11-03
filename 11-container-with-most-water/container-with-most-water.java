4971af37c695fadca795291cbcb97fbeea5a085d
Mohamed Sabith <47350620+sabith-nadakkavil@users.noreply.github.com>
2025-11-03 10:44:14 +0530

    Time: 5 ms (77.73%) | Memory: 77.1 MB (5.48%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -4,12 +4,10 @@ class Solution {
         int l = 0;
         int r = n - 1;
         int max = 0;
-
-        while (l < r){
+        while(l < r){
             int min = Math.min(height[l], height[r]);
             int curMax = min * (r - l);
-            max = Math.max(curMax, max);
-
+            max = Math.max(max, curMax);
             if(height[l] < height[r]){
                 l++;
             } else {
