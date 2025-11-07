63fce44367967797046e213258ec31121c3d1a4a
Mohamed Sabith <47350620+sabith-nadakkavil@users.noreply.github.com>
2025-11-07 16:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (6.77%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0, n = x;
+        while(n > 0){
+            sum += n%10;
+            n /= 10;
+        }
+        if(x % sum == 0){
+                return sum;
+            } 
+        return -1;
+    }
+}
